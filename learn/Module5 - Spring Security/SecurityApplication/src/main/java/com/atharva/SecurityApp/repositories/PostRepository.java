79aaaa7f400b8b7bd167005bda71678b88e19cf3
Atharva-web <ath3arv8a2@gmail.com>
2026-05-24 18:08:09 +0530

    spring security

---
+++ learn/Module5 - Spring Security/SecurityApplication/src/main/java/com/atharva/SecurityApp/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.atharva.SecurityApp.repositories;
+
+import com.atharva.SecurityApp.entities.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<PostEntity, Long> {
+}
---
+++ learn/Module5 - Spring Security/SecurityApplication/src/main/java/com/atharva/SecurityApp/services/PostService.java
@@ -0,0 +1,13 @@
+package com.atharva.SecurityApp.services;
+
+import com.atharva.SecurityApp.dto.PostDTO;
+import java.util.List;
+
+public interface PostService {
+
+    List<PostDTO> getAllPosts();
+
+    PostDTO createNewPost(PostDTO inputPost);
+
+    PostDTO getPostById(Long postId);
+}
